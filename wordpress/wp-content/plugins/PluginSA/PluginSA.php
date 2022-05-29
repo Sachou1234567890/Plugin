7d82d65f59c0277de929d0ad8456bed89b2363f8
Sachou1234567890 <sachasetruk@gmail.com>
2022-05-29 10:32:29 +0200

    Création du projet

---
+++ wordpress/wp-content/plugins/PluginSA/PluginSA.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: PluginSA
+Plugin URI: http://localhost:8888/plugin/wordpress/PLUGINSA/
+Description: Ceci un plugin pour la recupération de données sur Saleforce et Auth0
+Author: Sacha Setruk
+Version: 1.0
+*/
