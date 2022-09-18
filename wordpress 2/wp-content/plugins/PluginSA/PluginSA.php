0a2c8994b3dab9489b5b6cfef30dab000dbd3c19
Sachou1234567890 <sachasetruk@gmail.com>
2022-09-18 12:44:23 +0200

    Maj 2

--- wordpress 2/wp-content/plugins/PluginSA/PluginSA.php
+++ wordpress 2/wp-content/plugins/PluginSA/PluginSA.php
@@ -6,3 +6,8 @@ Description: Ceci un plugin pour la recupération de données sur Saleforce et A
 Author: Sacha Setruk
 Version: 1.0
 */
+
+$field = get_field_object('my_field');
+add_shortcode('notre_shortcode', $field);
+?>
+<p><?php echo $field['label']; ?>: <?php echo $field['value']; ?></p>
